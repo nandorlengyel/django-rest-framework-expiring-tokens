a572cac55111a29ba24983ff64d0a5439a15a15e
nandorlengyel <47507366+nandorlengyel@users.noreply.github.com>
2022-12-13 17:08:35 +0100

    Add HTTP_401_UNAUTHORIZED

--- rest_framework_expiring_authtoken/views.py
+++ rest_framework_expiring_authtoken/views.py
@@ -6,7 +6,7 @@ Classes:
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED
 
 from rest_framework_expiring_authtoken.models import ExpiringToken
 
@@ -22,20 +22,26 @@ class ObtainExpiringAuthToken(ObtainAuthToken):
         serializer = AuthTokenSerializer(data=request.data)
 
         if serializer.is_valid():
-            token, _ = ExpiringToken.objects.get_or_create(
-                user=serializer.validated_data['user']
-            )
-
-            if token.expired():
-                # If the token is expired, generate a new one.
-                token.delete()
-                token = ExpiringToken.objects.create(
+            try:
+                token, _ = ExpiringToken.objects.get_or_create(
                     user=serializer.validated_data['user']
                 )
 
-            data = {'token': token.key}
-            return Response(data)
+                if token.expired():
+                    # If the token is expired, generate a new one.
+                    token.delete()
+                    token = ExpiringToken.objects.create(
+                        user=serializer.validated_data['user']
+                    )
 
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+                data = {'token': token.key}
+                return Response(data)
+            except ExpiringToken.DoesNotExist:
+                return Response(
+                    {'error': 'User does not exist.'},
+                    status=HTTP_401_UNAUTHORIZED
+                )
+        return Response(serializer.errors, status=HTTP_401_UNAUTHORIZED)
+
 
 obtain_expiring_auth_token = ObtainExpiringAuthToken.as_view()
